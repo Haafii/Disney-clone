68d399ca99fa99c385c8c9f5512d97b8843e4857
Haafii <hafismuhammedtk@gmail.com>
2023-04-12 21:09:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+import Detail from "./components/Detail";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/detail" element={<Detail />} />
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
